a2c57a410607dd67f955eae9b735f2760ec1e57b
Marcos <marcoslipknot05@gmail.com>
2023-05-29 09:57:01 +0200

    first commit

---
+++ my-app/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import appLogo from "../images/MYtineraryLogo.png"
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>Landing Image</h1>
+      <img src={appLogo} alt="Descripción de la imagen" />
+    </div>
+  );
+};
+
+export default Landing;
