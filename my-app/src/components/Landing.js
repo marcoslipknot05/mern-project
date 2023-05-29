623165ab1c436c3634c534e2bb0033f233e2bf55
Marcos <marcoslipknot05@gmail.com>
2023-05-29 11:38:00 +0200

    create landing component, create router and add styles

--- my-app/src/components/Landing.js
+++ my-app/src/components/Landing.js
@@ -1,11 +1,54 @@
 import React from 'react';
 import appLogo from "../images/MYtineraryLogo.png"
+import "../utilities/landing.css";
+import imageLanding from "../images/start-landing-image.png";
+import { Link } from "react-router-dom";
+import homeImage from "../images/Home_logo.png";
+
+const userMenu = [
+  { name: 'Log in', link: '/LogIn' },
+  { name: 'Create Account', link: '/CreateAccount' },
+];
 
 const Landing = () => {
   return (
     <div>
-      <h1>Landing Image</h1>
-      <img src={appLogo} alt="Descripción de la imagen" />
+      <header>
+        <div>
+          <img src={appLogo} alt="Descripción de la imagen" className="logo-landing" />
+        </div>
+      </header>
+      <main>
+        <h1 className="title">
+          Find your perfect trip, designed by insiders who know and love their cities.
+        </h1>
+        <div className="start_browser">
+          <p className="browser_text">Start browsing</p>
+          <img
+            className="browser_image"
+            src={imageLanding}
+            alt={imageLanding}
+            style={{ width: "200px", height: "200px" }}
+          />
+        </div>
+        <div className="menu_container">
+          <h2>Want to build your own MYtinerary?</h2>
+          <div className="acces_menu">
+            {userMenu.map((menuItem, index) => (
+              <Link key={index} to={menuItem.link}>
+                <li>{menuItem.name}</li>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </main>
+      <footer>
+        <img
+          src={homeImage}
+          alt={homeImage}
+          style={{ width: "50px", height: "50px" }}
+        />
+      </footer>
     </div>
   );
 };
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
 import reportWebVitals from './reportWebVitals'; 
 import Landing from './components/Landing';
+import LogIn from "./components/LogIn";
+import CreateAccount from "./components/CreateAccount";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+  },
+  {
+    path: "/login",
+    element: <LogIn />,
+  },
+  {
+    path: "/createaccount",
+    element: <CreateAccount />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Landing />
+    <RouterProvider router={router}>
+      <Landing />
+    </RouterProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
