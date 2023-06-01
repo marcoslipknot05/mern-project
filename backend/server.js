5f4b869620fad60024346b8db704dd2a315d498f
marcoslipknot05 <marcoslipknot05@gmail.com>
2023-06-01 12:52:32 +0200

    backend modified

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log("Server is running on" + port + "por");
-});
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(bodyParser.jason());
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+app.use(cors());
