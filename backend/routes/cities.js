ba86a4587ec65ff5f6648015967a583429deea31
marcoslipknot05 <marcoslipknot05@gmail.com>
2023-06-05 18:38:59 +0200

    test route with postman

--- backend/routes/cities.js
+++ backend/routes/cities.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/test', (req, res) => {
-  res.send({ msg: 'Cities test route.'});
+const citiesData = [
+  { id: 1, name: 'Ciudad 1' },
+  { id: 2, name: 'Ciudad 2' },
+  { id: 3, name: 'Ciudad 3' }
+];
+
+router.get('/', (req, res) => {
+  res.send(citiesData);
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
-app.use('/cities', require('./routes/cities'))
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-app.use(bodyParser.jason());
-app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
-);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use('/cities', require('./routes/cities'));
+
+app.listen(3000, () => {
+  console.log('Servidor en ejecución en el puerto 3000');
+});
